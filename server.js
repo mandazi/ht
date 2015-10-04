1b95ceb9765fe2054242434487b0a58c8ebfab43
Amir Sahib <amir@Amir-Sahibs-MBP.home>
2015-10-04 12:05:03 -0400

    Add so it begins...

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+var express = require('express');
+var	app = express();
+var bodyParser = require('body-parser');
+var Parse = require('parse').Parse;
+
+// define the port
+var port = process.env.PORT || 8080;
